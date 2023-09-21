af07ba26540f770a87caa0b043ff07abaefa812f
Artem Shubovych <shybovycha@gmail.com>
2023-09-21 17:26:26 +1000

    Start rework with editor and blank glfw window

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,14 +1,40 @@
-#include "Application.h"
+#include <GLFW/glfw3.h>
 
-#ifdef _IRR_WINDOWS_
-#pragma comment(lib, "irrlicht.lib")
-#endif
-
-int main()
+int main(void)
 {
-    std::unique_ptr<Application> app = std::make_unique<Application>();
+    GLFWwindow* window;
 
-    app->run();
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
