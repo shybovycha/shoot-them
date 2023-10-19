c5b1a1354dba5af199644c441e3e3fde6c5e1eed
Artem Shubovych <shybovycha@gmail.com>
2023-10-19 20:24:55 +1100

    Split out common logic into separate lua scripts

---
+++ game/resources/scenes/common.lua
@@ -0,0 +1,65 @@
+function load_common_models()
+    resources:add_3d_model("chicken", "resources/models/old/chicken.3ds")
+    resources:add_texture("chicken_spec", "resources/models/old/Chick03.BMP")
+
+    -- resources:add_framebuffer("rtt")
+end
+
+function init_camera()
+    scene_data.camera = Camera(
+        Vec3(0, 0, 0), -- position
+        Vec3(0, 1, 0), -- up
+        Vec3(0, 0, -1), -- forward
+        Vec3(1, 0, 0) -- right
+    )
+
+    scene_data.mouse_cursor_pos = inputs.mouse_position
+end
+
+function update_camera(delta_time)
+    -- update mouse position
+    current_mouse_pos = inputs.mouse_position
+    mouse_delta = scene_data.mouse_cursor_pos - current_mouse_pos
+    scene_data.mouse_cursor_pos = current_mouse_pos
+
+    scene_data.camera:rotate(mouse_delta.x, mouse_delta.y)
+end
+
+function process_inputs()
+    -- check mouse button press
+    if inputs:is_mouse_button_pressed(MOUSE_BUTTON_LEFT) then
+    end
+
+    if inputs:is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) then
+    end
+end
+
+function render_targets(delta_time)
+    -- render scene
+    renderer:use_shader_program("default")
+    renderer:use_framebuffer("default")
+
+    renderer:set_uniform("view", scene_data.camera.view_matrix)
+    renderer:set_uniform("projection", scene_data.camera.projection_matrix)
+
+    for target in scene_data.targets do
+        renderer:set_texture("specular_texture", "chicken_spec")
+        renderer:set_uniform("model", target["position"])
+
+        renderer:draw_3d_model("chicken")
+    end
+end
+
+function render_scene(delta_time, render_level_fn)
+    renderer:use_shader_program("default")
+    renderer:use_framebuffer("default")
+
+    renderer:set_uniform("view", scene_data.camera.view_matrix)
+    renderer:set_uniform("projection", scene_data.camera.projection_matrix)
+
+    if render_level_fn then
+        render_level_fn(delta_time)
+    end
+
+    render_targets(delta_time)
+end
--- game/resources/scenes/level1.lua
+++ game/resources/scenes/level1.lua
@@ -1,10 +1,9 @@
+require "resources/scenes/common"
+
 function on_load()
+    load_common_models()
+
     resources:add_3d_model("room", "resources/models/old/room1.dae")
-
-    resources:add_3d_model("chicken", "resources/models/old/chicken.3ds")
-    resources:add_texture("chicken_spec", "resources/models/old/Chick03.BMP")
-
-    -- resources:add_framebuffer("rtt")
 
     scene_data.targets = {
         { position = Vec3(-765.06158447265625, 271.0589599609375, 670.49334716796875) },
@@ -12,47 +11,20 @@ function on_load()
         { position = Vec3(746.4915771484375, 291.43478393554688, 613.66107177734375) },
     }
 
-    scene_data.camera = Camera(
-        Vec3(0, 0, 0), -- position
-        Vec3(0, 1, 0), -- up
-        Vec3(0, 0, -1), -- forward
-        Vec3(1, 0, 0) -- right
-    )
+    init_camera()
+end
 
-    scene_data.mouse_cursor_pos = inputs.mouse_position
+function render_level(delta_time)
+    renderer:set_uniform("model", Vec3(0, 0, 0))
+    renderer:draw_3d_model("room")
 end
 
 function on_render(delta_time)
-    -- update mouse position
-    current_mouse_pos = inputs.mouse_position
-    mouse_delta = scene_data.mouse_cursor_pos - current_mouse_pos
-    scene_data.mouse_cursor_pos = current_mouse_pos
+    update_camera(delta_time)
 
-    scene_data.camera:rotate(mouse_delta.x, mouse_delta.y)
+    process_inputs()
 
-    -- check mouse button press
-    if inputs:is_mouse_button_pressed(MOUSE_BUTTON_LEFT) then
-    end
-
-    if inputs:is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) then
-    end
-
-    -- render scene
-    renderer:use_shader_program("default")
-    renderer:use_framebuffer("default")
-
-    renderer:set_uniform("view", scene_data.camera.view_matrix)
-    renderer:set_uniform("projection", scene_data.camera.projection_matrix)
-
-    renderer:set_uniform("model", Vec3(0, 0, 0))
-    renderer:draw_3d_model("room")
-
-    for target in scene_data.targets do
-        renderer:set_texture("specular_texture", "chicken_spec")
-        renderer:set_uniform("model", target["position"])
-
-        renderer:draw_3d_model("chicken")
-    end
+    render_scene(delta_time, render_level)
 
     -- renderer:add_light(Vec3(-717.96405029296875, 129.1875, 200), Vec3(255, 255, 255) 100.0)
     -- renderer:add_light(Vec3(0, 125.34170532226563, 258.18295288085938), Vec3(255, 255, 255) 100.0)
